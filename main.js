9e701bb01c96e2a6711629766519e0e959d46f3a
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-04-26 19:45:47 -0500

    26 - 04 - 2021

---
+++ main.js
@@ -0,0 +1,62 @@
+
+
+// this.nombre = "Miguel Angel";
+// console.log(this.nombre);
+
+
+// function saludar(){
+//     var apelldio2 = "CASTRO ESCAMILLA";
+//     let apelldio3 = "castro escamilla";
+//     this.apellidos = "Castro Escamilla";
+// }
+// saludar();
+// console.log(this.apellidos);
+// console.log(apelldio2);
+// console.log(apelldio3);
+
+// Maneras de declarar objetos
+// const obj =  new Object();
+// const obj2 = {};
+// const obj3 =  Object.prototype.constructor();
+
+
+// const lista2 = new Object();
+// lista2.nombre = "Miguel";
+// lista2.apellido = "Escamilla";
+
+
+
+const lista = {
+    nombre: "Miguel Angel",
+    apellido: "Castro Escamilla",
+    mostrar: p1 => `Hola ${p1} como estas`,
+};
+function saludar(Callback,p2){
+    // console.log(this.mostrar(this.nombre));
+    console.log(Callback(p2));
+}
+// saludar.call(lista, lista.mostrar, lista.nombre);
+// saludar.apply(lista, [lista.mostrar, lista.nombre]);
+const ejecutar = saludar.bind(lista, lista.mostrar, lista.nombre);
+ejecutar();
+
+
+
+
+
+
+// const inner = ( function (){
+//     let num = 0;
+//     return function () {
+//         num++;
+//         return num;
+//     }
+// })();
+
+// console.log(inner());
+// console.log(inner());
+
+
+
+
+
